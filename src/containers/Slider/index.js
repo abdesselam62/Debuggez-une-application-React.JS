4a1481d444d1ad1d5558494c38085346a633906a
Abdesselam62 <slamusdr@hotmail.com>
2023-12-25 18:46:51 +0100

    ajout de la fonction handleRadioChange pour gérer le changement de radio

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,56 +7,51 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
- 
 
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
+
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), // incrémentation de-1 oublié a index < byDateDesc.length -1
-      5000
-    );
+    setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
   };
+
+ // ajout de la fonction  handleRadioChange pour gérer le changement de radio
+ const handleRadioChange = (newIndex) => {
+  setIndex(newIndex);
+};
+
   useEffect(() => {
     nextCard();
   });
-  
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        
-    <>
-          <div
-            key={event.date} // Utilisation de l'attribut "date" comme clé
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-            
-          >
-            <img src={event.cover} alt="forum" />
-    <div className="SlideCard__descriptionContainer">
-      <div className="SlideCard__description">
-        <h3>{event.title}</h3>
-        <p>{event.description}</p>
-        <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div key={event.id} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </> 
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((event, radioIdx) => (
+            <input
+              key={event.id} // Utilisation de  l'ID de l'événement focus comme clé unique
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => handleRadioChange(radioIdx)} // Ajout  d'un gestionnaire onChange
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
